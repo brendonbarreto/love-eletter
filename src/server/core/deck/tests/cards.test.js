22c889d20bb7039265c07119b717c88e9e09ac84
Brendon Barreto <brendondominic@gmail.com>
2021-03-18 22:07:45 -0300

    Creating cards module (#2)
    * Creating module and tests
    
    * More tests to card module

--- src/server/core/deck/tests/cards.test.js
+++ src/server/core/deck/tests/cards.test.js
@@ -1,8 +1,59 @@
-import cards from '../cards';
+import { Character, getCardByCharacter, is, getCardByValue, getCards, getSpy } from '../cards';
 
-it('should includes Spy', () => {
-  const spy = cards.find(a => a.name === 'Spy')
 
-  expect(spy.level).toBe(0)
-  expect(spy.copies).toBe(2)
-});
+describe('cards', () => {
+  describe('getCardByCharacter', () => {
+    it.each(Object.values(Character))('should return %p', character => {
+      const card = getCardByCharacter(character)
+    
+      expect(is(card, character)).toBe(true)
+    })
+  })
+
+  describe('getCardByValue', () => {
+    it.each(getCards().map(card => [card.character, card.value]))('should return %p when value is %p', (character, value) => {
+      const card = getCardByValue(value)
+    
+      expect(is(card, character)).toBe(true)
+    })
+  })
+
+  describe('getCards', () => {
+    it.each(Object.values(Character))('should include %p card', character => {
+      const cards = getCards()
+    
+      expect(cards.map(card => card.character)).toContain(character)
+    })
+  })
+
+  describe('is', () => {
+    const differentKeysCases = [
+      ['character', 'definitely not a spy'],
+      ['copies', 3],
+      ['value', 1]
+    ]
+    it.each(differentKeysCases)('should return false when %p keys are different', (key, value) => {
+      const fakeSpy = { ...getSpy(), [key]: value }
+
+      const result = is(fakeSpy, Character.SPY)
+
+      expect(result).toBe(false)
+    })
+
+    it('should return true if character, copies and value keys are the same', () => {
+      const spy = { character: Character.SPY, copies: 2, value: 0 }
+
+      const result = is(spy, Character.SPY)
+
+      expect(result).toBe(true)
+    })
+
+    it('should return false when required key is not present', () => {
+      const spy = { character: Character.SPY, copies: 2 }
+
+      const result = is(spy, Character.SPY)
+
+      expect(result).toBe(false)
+    })
+  })
+})
