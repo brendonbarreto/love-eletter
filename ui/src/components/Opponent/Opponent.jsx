45ef7569c16dfc938bcad4cedf1a732e21deb350
Brendon Barreto <brendondominic@gmail.com>
2021-04-09 20:41:28 -0300

    card rules

--- ui/src/components/Opponent/Opponent.jsx
+++ ui/src/components/Opponent/Opponent.jsx
@@ -4,13 +4,13 @@ import FaceUpCards from '../FaceUpCards'
 const Opponent = () => (
   <div className={styles.opponent}>
     <div className={styles.avatarAndTokens}>
-      <div className={styles.avatar}>BR</div>
+      <div className={styles.avatar}>EO</div>
       <div className={styles.score}>
         <div className={styles.token}/>
         <div className={styles.token}/>
       </div>
     </div>
-    <p>Brendon Ribeiro</p>
+    <p>Édina de Oliveira</p>
     <FaceUpCards />
   </div>
 )
