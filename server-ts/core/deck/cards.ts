9a4f92832d083160d51cf707d1065707ce4f1b72
Brendon Barreto <brendondominic@gmail.com>
2021-05-21 14:44:14 -0300

    tests with ts

--- server-ts/core/deck/cards.ts
+++ server-ts/core/deck/cards.ts
@@ -1,3 +1,5 @@
+import { isDeepStrictEqual } from 'util';
+
 export enum Character {
   SPY = 'Spy',
   GUARD = 'Guard',
@@ -21,105 +23,67 @@ export interface Card {
   value: CardValues;
 }
 
-export const getSpy = () => ({
+export const getSpy = (): Card => ({
   character: Character.SPY,
   copies: 2,
   value: 0
 })
 
-export const getGuard = () => ({
+export const getGuard = (): Card => ({
   character: Character.GUARD,
-  rules: [
-    'Choose another player and name a character other than Guard. If the chosen player has that card in their hand, they are out of the round.'
-  ],
   copies: 6,
   value: 1
 })
 
-export const getPriest = () => ({
+export const getPriest = (): Card => ({
   character: Character.PRIEST,
-  rules: [
-    'Choose another player and secretly look at their hand (without revealing it to anyone else).'
-  ],
   copies: 2,
   value: 2
 })
 
-export const getBaron = () => ({
+export const getBaron = (): Card => ({
   character: Character.BARON,
-  rules: [
-    'Choose another player. You and that player secretly compare your hands. Whoever has the lower value card is out of the round.',
-    'If there is a tie, neither player is out of the round'
-  ],
   copies: 2,
   value: 3
 })
 
-export const getHandmaid = () => ({
+export const getHandmaid = (): Card => ({
   character: Character.HANDMAID,
-  rules: [
-    'Until the start of your next turn, other players cannot choose you for their card effects.',
-    'In the rare case that all other players still in the round are "protected" by a Handmaid when you play a card, do the following:',
-    '* If that card requires you to choose another player (Guard, Priest, Baron, King), your card is played with no effect.',
-    'If that card requires you to choose any player (Prince), then you must choose yourself for the effect.'
-  ],
   copies: 2,
   value: 4
 })
 
-export const getPrince = () => ({
+export const getPrince = (): Card => ({
   character: Character.PRINCE,
-  rules: [
-    'Choose any player (including yourself). That player discards their hand (without resolving its effect) and draws a new hand.',
-    'If the deck is empty, the chosen player draws the facedown set-aside card.',
-    'If a player chooses you for the Prince effect, and you are forced to discard the Princess, you are out of the round.'
-  ],
   copies: 2,
   value: 5
 })
 
-export const getChancellor = () => ({
+export const getChancellor = (): Card => ({
   character: Character.CHANCELLOR,
-  rules: [
-    'Draw two cards from the deck into your hand. Choose and keep one of the three cards now in your hand, and place the other two facedown on the bottom of the deck in any order.',
-    'If there is only one card in the deck, draw it and return one card. If there are no cards left, this card is played with no effect.'
-  ],
   copies: 2,
   value: 6
 })
 
-export const getKing = () => ({
+export const getKing = (): Card => ({
   character: Character.KING,
-  rules: [
-    'Choose another player and trade hands with that player.'
-  ],
   copies: 1,
   value: 7
 })
 
-export const getCountess = () => ({
+export const getCountess = (): Card => ({
   character: Character.COUNTESS,
-  rules: [
-    'The Countess has no effect when played or discarded.',
-    'You must play the Countess as the card for your turn if either the King or a Prince is the other card in your hand.',
-    'You can still choose to play the Countess as the card for your turn even if you do not have the King or a Prince.',
-    'When you play the Countess, do not reveal your other card; the other players will not know if you were forced to play it or chose to play it.',
-    'The Countess’s effect does not apply when you draw cards for other effects (Chancellor).'
-  ],
   copies: 1,
   value: 8
 })
 
-export const getPrincess = () => ({
+export const getPrincess = (): Card => ({
   character: Character.PRINCESS,
-  rules: [
-    'If you either play or discard the Princess for any reason, you are immediately out of the round.'
-  ],
   copies: 1,
   value: 9
 })
 
-export const getCards = () => [
+export const getCards = (): Card[] => [
   getSpy(),
   getGuard(),
   getPriest(),
@@ -132,15 +96,22 @@ export const getCards = () => [
   getPrincess()
 ]
 
-export const is = (card: Card, character: Character) => {
+export const is = (card: Card | undefined, character: Character) => {
   const cardToCompare = getCardByCharacter(character)
-  if (!cardToCompare) return false
 
-  return card.character === cardToCompare.character
-      && card.copies === cardToCompare.copies
-      && card.value === cardToCompare.value
+  return isDeepStrictEqual(card, cardToCompare)
 }
 
-export const getCardByCharacter = (character: Character) => getCards().find(card => card.character === character)
+export const getCardByCharacter = (character: Character): Card => {
+  const card = getCards().find(card => card.character === character)
+  if (card === undefined) throw new Error(`Could not find a card for ${character} character.`)
 
-export const getCardByValue = (value: CardValues) => getCards().find(card => card.value === value)
+  return card
+}
+
+export const getCardByValue = (value: CardValues): Card => {
+  const card = getCards().find(card => card.value === value)
+  if (card === undefined) throw new Error(`Could not find a card with value ${value}.`)
+
+  return card
+}
---
+++ server-ts/core/deck/tests/cards.test.ts
@@ -0,0 +1,69 @@
+import {
+  Character,
+  getCardByCharacter,
+  is,
+  getCardByValue,
+  getCards,
+  CardValues,
+  Card
+} from '../cards'
+
+describe('cards', () => {
+  describe('getCardByCharacter', () => {
+    it.each(Object.values(Character))('should return %p', character => {
+      const card = getCardByCharacter(character)
+
+      expect(is(card, character)).toBe(true)
+    })
+  })
+
+  describe('getCardByValue', () => {
+    it.each(getCards().map((card): [Character, CardValues] => [card.character, card.value]))('should return %p when value is %p', (character, value) => {
+      const card = getCardByValue(value)
+
+      expect(is(card, character)).toBe(true)
+    })
+  })
+
+  describe('getCards', () => {
+    it.each(Object.values(Character))('should include %p card', character => {
+      const cards = getCards()
+
+      expect(cards.map(card => card.character)).toContain(character)
+    })
+  })
+
+  describe('is', () => {
+    it('should return true if character, copies and value keys are the same', () => {
+      const spy: Card = { character: Character.SPY, copies: 2, value: 0 }
+
+      const result = is(spy, Character.SPY)
+
+      expect(result).toBe(true)
+    })
+
+    it('should return false if characters are differents', () => {
+      const spy: Card = { character: Character.GUARD, copies: 2, value: 0 }
+
+      const result = is(spy, Character.SPY)
+
+      expect(result).toBe(false)
+    })
+
+    it('should return false if copies are differents', () => {
+      const spy: Card = { character: Character.SPY, copies: 1, value: 0 }
+
+      const result = is(spy, Character.SPY)
+
+      expect(result).toBe(false)
+    })
+
+    it('should return false if values are differents', () => {
+      const spy: Card = { character: Character.SPY, copies: 2, value: 1 }
+
+      const result = is(spy, Character.SPY)
+
+      expect(result).toBe(false)
+    })
+  })
+})
